406d4b424834ee2a8b895fe00ed03a17df9d2e24
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-02-25 20:15:29 +0530

    Add MainLogo component and update CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import './App.css'
 
+
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+    <div className='main_app_container h-[100vh] w-[100vw]'>
+      <div className='w-full bg-[#F3F3F3]'>
+        <div className='max-w-[1250px] h-[80px] mx-auto  flex justify-start items-center'>
+          <div className='text-[36px]'>
+            <span className="text-[#387ADF] font-[900]">Acme</span>
+            <span>&nbsp;Note</span>
+          </div>
+          <div className='flex-1'></div>
+          <div>-{">"}Start writing Notes</div>
+        </div>
+      </div>
+      <h1 className="text-3xl font-bold">
+      </h1>
+    </div>
   )
 }
 

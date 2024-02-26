102310ef1cd06c6d5ed6a5895235e602cb29ada1
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-02-26 21:56:08 +0530

    Refactor App component and add About component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Navbar from './components/navbar/Navbar';
 import HeroSec from './components/herosec/HeroSec';
-import image1 from './assets/img1.webp'
+import About from './components/aboutsec/About';
 
 
 function App() {
@@ -11,22 +11,7 @@ function App() {
       <Navbar />
       <div className='contents_container w-full max-w-[1128px] h-[80px] mx-auto '>
         <HeroSec />
-        <div className='about '>
-          <div className='row_1 flex items-center'>
-            <div className='Child1 w-auto max-w-[400px]'>
-              <div className='text-4xl font-extrabold'>
-                A handwriting experience like no other.
-              </div>
-              <div className='text-xl font-light tracking-wide'>
-                Fall in love with handwriting all over again with Goodnotes’ digital ink. ❤️ by millions of people worldwide.
-              </div>
-            </div>
-            <div className='child2 flex-1'></div>
-            <div className='image w-auto max-w-[508px]'>
-              <img className='rounded'src={image1}></img>
-            </div>
-          </div>
-        </div>
+        <About/>
       </div>
     </div>
   )
---
+++ src/components/aboutsec/About.tsx
@@ -0,0 +1,55 @@
+import image1 from '../../assets/img1.webp';
+
+type RowItemProps = {
+    title: string,
+    description: string;
+    image: string;
+    reverse?: boolean;
+}
+
+const RowData: RowItemProps[] = [
+    {
+        title: 'A handwriting experience like no other.',
+        description: 'Fall in love with handwriting all over again with Goodnotes’ digital ink. ❤️ by millions of people worldwide.',
+        image: image1
+    }, {
+        title: 'Write freely on paper that recognizes every typo.',
+        description: 'Powerful AI features like Spellcheck and Word Complete fix your mistakes, so you can focus on getting your ideas on the page.',
+        image: image1,
+        reverse: true
+    }, {
+        title: 'Your entire library, just a click away',
+        description: 'Whether you’re typing away on your laptop, annotating a PDF on your tablet, or reviewing your notes on your phone, you can do it with Goodnotes. Now available on every device.',
+        image: image1
+    }
+]
+
+const RowItem = (props: RowItemProps) => {
+    return (
+        <div className={`row_1 flex ${props.reverse? "flex-row-reverse":""} items-center py-12`}>
+            <div className='Child1 w-auto max-w-[400px]'>
+                <div className='text-4xl font-extrabold'>
+                    {props.title}
+                </div>
+                <div className='text-xl font-light tracking-wide'>
+                    {props.description}
+                </div>
+            </div>
+            <div className='child2 flex-1'></div>
+            <div className='image w-auto max-w-[508px]'>
+                <img className='rounded' src={props.image}></img>
+            </div>
+        </div>
+    )
+}
+
+const About = () => {
+    return (
+        <div className='about '>
+            {RowData.map((item, index) =>
+                <RowItem key={index} {...item} />)}
+        </div>
+    )
+}
+
+export default About

265649c1a89e40e32ad68f4627b45371e7a231c5
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-02-28 00:07:42 +0530

    Add new images and update footer layout

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="py-20 bg-black text-white min-[0px]:px-4 max-[1350px]:px-0">
-            <div className="w-full max-w-[1225px] mx-auto flex justify-center">
+            <div className="w-full max-w-[1225px] mx-auto flex justify-center flex-col gap-10 md:flex-row md:gap-0">
                 <div className='text-[36px] flex-1'>
                     <span className="text-[#387ADF] font-[900]">Acme</span>
                     <span className="font-extralight">&nbsp;Note</span>
